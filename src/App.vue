31c089615a0a2542d8e334feca18f1de23816d39
Prokofif <prokofev.danil22@gmail.com>
2022-11-04 09:25:46 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NerdflixMovies/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script> 
+import NerdflixMovies from './components/NerdflixMovies.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NerdflixMovies
   }
 }
 </script>
